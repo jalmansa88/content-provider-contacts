579cdba1bf310e61eb1b60601fbf42f755a6a4cc
almansaj <javier.almansa@easypaymentgateway.com>
2017-07-16 11:16:09 +0200

    functional version

--- app/src/main/java/com/lordneru/pruebas/contentsproviders/MainActivity.java
+++ app/src/main/java/com/lordneru/pruebas/contentsproviders/MainActivity.java
@@ -1,13 +1,62 @@
 package com.lordneru.pruebas.contentsproviders;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button boton;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getData();
+            }
+
+
+        });
+    }
+
+    private void getData() {
+
+        String[] projection = new String[]{
+                ContactsContract.Data._ID,
+                ContactsContract.Data.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.TYPE};
+
+        String selectionClause = ContactsContract.Data.MIMETYPE + "='" +
+                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "' AND " +
+                ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL";
+        String sortOrder = ContactsContract.Data.DISPLAY_NAME + " ASC";
+
+        Cursor c = getContentResolver().query(
+                ContactsContract.Data.CONTENT_URI,
+                projection,
+                selectionClause,
+                null,
+                sortOrder
+        );
+
+        textView.setText("");
+
+        while(c.moveToNext()){
+            textView.append("Id " + c.getString(0) + "Name: " + c.getString(1) + "Phone: " + c.getString(2) + "Type: " + c.getString(3));
+        }
+
     }
 }
